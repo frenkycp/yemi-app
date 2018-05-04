4c4716042c7f7d21e777d8d7026a278846fe41ab
Frenky Cahya Purnama (YEMI) <frenky.purnama@music.yamaha.com>
2018-05-04 14:36:24 +0700

    - change redirect on login

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -39,7 +39,8 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
-        return $this->render('index');
+        //return $this->render('index');
+        return $this->redirect(['weekly-plan/index']);
     }
 
     public function actionProfile()
