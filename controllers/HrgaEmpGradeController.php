eae8b8c4adff88237b515aa84737953945b34e0f
Frenky Cahya Purnama (YEMI) <frenky.purnama@music.yamaha.com>
2018-06-06 14:39:19 +0700

    - add hrga chart employee qty by grade

---
+++ controllers/HrgaEmpGradeController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+use app\models\MpInOut;
+
+class HrgaEmpGradeController extends Controller
+{
+	public function behaviors()
+    {
+        //apply role_action table for privilege (doesn't apply to super admin)
+        return \app\models\Action::getAccess($this->id);
+    }
+    
+    public function actionIndex()
+    {
+    	$title = 'Employee Grade Quantity';
+    	$subtitle = '';
+    	$data = [];
+    	$emp_data_male = [];
+    	$emp_data_female = [];
+
+    	$sr_grade = [
+    		'G1', 'G2',
+    		'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8',
+    		'L1', 'L2', 'L3', 'L4',
+    		'M1', 'M2', 'M3', 'M4', 'M5', 'M6', 'M7', 'M8',
+    		'D1', 'D2', 'D3'
+    	];
+
+    	$categories = [
+		    '0-4', '5-9', '10-14', '15-19',
+		    '20-24', '25-29', '30-34', '35-39', '40-44',
+		    '45-49', '50-54', '55-59', '60-64', '65-69',
+		    '70-74', '75-79', '80-84', '85-89', '90-94',
+		    '95-99', '100 + '
+		];
+
+		$jk_arr = ['L', 'P'];
+
+		$emp_data = MpInOut::find()
+		->select([
+			'GRADE' => 'GRADE',
+			'JENIS_KELAMIN' => 'JENIS_KELAMIN',
+			'total_emp' => 'COUNT(*)'
+		])
+		->where([
+			'TANGGAL' => date('Y-m-d')
+		])
+		->groupBy('GRADE, JENIS_KELAMIN')
+		->all();
+
+		foreach ($jk_arr as $jk) {
+			$tmp_data = [];
+			foreach ($sr_grade as $grade) {
+				$tmp_qty = null;
+				foreach ($emp_data as $value) {
+					$tmp_grade = $value->GRADE;
+					$tmp_jk = $value->JENIS_KELAMIN;
+					if ($tmp_grade == $grade && $tmp_jk == $jk) {
+						$tmp_qty = (int)$value->total_emp;
+						if ($jk == 'L') {
+							$tmp_qty = (-1 * $tmp_qty);
+						}
+					}
+				}
+				$tmp_data[] = $tmp_qty;
+			}
+			$data[] = [
+				'name' => $jk == 'L' ? 'Male' : 'Female',
+				'data' => $tmp_data
+			];
+		}
+		
+
+    	return $this->render('index', [
+    		'title' => $title,
+    		'subtitle' => $subtitle,
+    		'categories' => $sr_grade,
+    		'data' => $data
+    	]);
+    }
+}
---
+++ views/hrga-emp-grade/index.php
@@ -0,0 +1,126 @@
+<?php
+use miloschuman\highcharts\Highcharts;
+use yii\web\JsExpression;
+use yii\web\View;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+$this->title = Yii::t('app', 'Employee Chart Grade Quantity');
+$this->params['breadcrumbs'][] = $this->title;
+$color = 'ForestGreen';
+
+date_default_timezone_set('Asia/Jakarta');
+
+$this->registerCss(".tab-content > .tab-pane,
+.pill-content > .pill-pane {
+    display: block;     
+    height: 0;          
+    overflow-y: hidden; 
+}
+
+.tab-content > .active,
+.pill-content > .active {
+    height: auto;       
+} ");
+
+$script = <<< JS
+    window.onload = setupRefresh;
+
+    function setupRefresh() {
+      setTimeout("refreshPage();", 600000); // milliseconds
+    }
+    function refreshPage() {
+       window.location = location.href;
+    }
+JS;
+$this->registerJs($script, View::POS_HEAD );
+
+?>
+<?php
+//echo '<pre>';
+//print_r($data);
+//echo '</pre>';
+?>
+<div class="box box-success">
+    <div class="box-header with-border">
+        <h3 class="box-title"><?= date('d-M-Y') ?></h3>
+    </div>
+    <div class="box-body no-padding">
+        <?php
+        echo Highcharts::widget([
+            'scripts' => [
+                'modules/exporting',
+                //'themes/grid-light',
+                'themes/sand-signika',
+                'themes/dark-unica',
+            ],
+            'options' => [
+                'chart' => [
+                    'type' => 'bar',
+                    'height' => 550,
+                ],
+                'credits' => [
+                    'enabled' =>false
+                ],
+                'title' => [
+                    'text' => $title
+                ],
+                'subtitle' => [
+                    'text' => $subtitle
+                ],
+                'xAxis' => [
+                    [
+                        'categories' => $categories,
+                        'reversed' => false,
+                        'labels' => [
+                            'step' => 1
+                        ]
+                    ],
+                    [ // mirror axis on right side
+                        'opposite' => true,
+                        'reversed' => false,
+                        'categories' => $categories,
+                        'linkedTo' => 0,
+                        'labels' => [
+                            'step' => 1
+                        ]
+                    ]
+                    
+                ],
+                'yAxis' => [
+                    [
+                        'title' => [
+                            'text' => null
+                        ],
+                        'labels' => [
+                            'formatter' => new JsExpression("function () { return Math.abs(this.value) + '' }")
+                        ],
+                        'max' => 450
+                    ],
+                    [
+                        'title' => [
+                            'text' => null
+                        ],
+                        'labels' => [
+                            'formatter' => new JsExpression("function () { return Math.abs(this.value) + '' }")
+                        ],
+                        'max' => 450
+                    ]
+                ],
+                'tooltip' => [
+                    'formatter' => new JsExpression("function () {
+                        return '<b>' + this.series.name + ', Grade ' + this.point.category + '</b><br/>' +
+                            'Total: ' + Highcharts.numberFormat(Math.abs(this.point.y), 0);
+                    }")
+                ],
+                'plotOptions' => [
+                    'series' => [
+                        'stacking' => 'normal'
+                    ]
+                ],
+                'series' => $data,
+            ],
+        ]);
+        ?>
+    </div>
+</div>
