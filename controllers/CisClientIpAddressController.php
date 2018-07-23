c04c922bafc6a3c3fd6e79dc23a17d9c0b944dc5
Frenky Cahya Purnama (YEMI) <frenky.purnama@music.yamaha.com>
2018-07-23 14:03:36 +0700

    CI Client IP Address add behavior

--- controllers/CisClientIpAddressController.php
+++ controllers/CisClientIpAddressController.php
@@ -7,5 +7,9 @@ namespace app\controllers;
 */
 class CisClientIpAddressController extends \app\controllers\base\CisClientIpAddressController
 {
-
+	public function behaviors()
+    {
+        //apply role_action table for privilege (doesn't apply to super admin)
+        return \app\models\Action::getAccess($this->id);
+    }
 }
