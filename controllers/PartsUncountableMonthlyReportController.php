57bfdcbddfe7d8789268845df94a4b69c36e2588
FRENKY-GLORIA\Frenky - Gloria <friendq.c.p@gmail.com>
2018-09-21 09:20:19 +0700

    - budomari add behavior

--- controllers/PartsUncountableMonthlyReportController.php
+++ controllers/PartsUncountableMonthlyReportController.php
@@ -14,6 +14,12 @@ use yii\helpers\Url;
  */
 class PartsUncountableMonthlyReportController extends Controller
 {
+	public function behaviors()
+    {
+        //apply role_action table for privilege (doesn't apply to super admin)
+        return \app\models\Action::getAccess($this->id);
+    }
+    
 	public function actionIndex()
 	{
 		$data = [];
