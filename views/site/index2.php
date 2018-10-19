eaaa8f374bebefa171ccdbda4188656b1b7a2eae
Frenky Cahya Purnama (YEMI) <frenky.purnama@music.yamaha.com>
2018-10-19 15:56:06 +0700

    -add link

--- views/site/index2.php
+++ views/site/index2.php
@@ -51,10 +51,10 @@ $this->registerCss("
                             <p>Vendor to YEMI <span class="japanesse-word">(ベンダー⇒YEMI)</span></p>
                             <ol style="padding-left: 1em">
                                 <li>
-                                    <?= Html::a('Weekly MilkRun Parts <span class="japanesse-word">(週次ミルクラン部品納入）</span>', ['/parts-milk-run-weekly/index']); ?>
+                                    <?= Html::a('Weekly MilkRun Parts <span class="japanesse-word">(週次ミルクラン部品納入）</span>', ['/parts-milk-run-weekly']); ?>
                                 </li>
                                 <li>
-                                    <?= Html::a('Weekly JIT Parts <span class="japanesse-word">（週次JIT部品納入)</span>', ['/parts-jit-weekly/index']); ?>
+                                    <?= Html::a('Weekly JIT Parts <span class="japanesse-word">（週次JIT部品納入)</span>', ['/parts-jit-weekly']); ?>
                                 </li>
                             </ol>
                         </div>
@@ -62,10 +62,13 @@ $this->registerCss("
                             <p>YEMI WH to Production<br/><span class="japanesse-word">(YEMI 部品倉庫⇒生産職場)</span></p>
                             <ol style="padding-left: 1em">
                                 <li>
-                                    <?= Html::a('Weekly Picking Status <span class="japanesse-word">(週次ピッキング状況)</span>', ['parts-picking-status/index']); ?>
+                                    <?= Html::a('Weekly Picking Status <span class="japanesse-word">(週次ピッキング状況)</span>', ['parts-picking-status']); ?>
                                 </li>
                                 <li>
-                                    <?= Html::a('Budomari Material Monitor <span class="japanesse-word">(材料歩留モニター）</span>', ['parts-uncountable-monthly-report/index']); ?>
+                                    <?= Html::a('Budomari Material Monitor <span class="japanesse-word">(材料歩留モニター）</span>', ['parts-uncountable-monthly-report']); ?>
+                                </li>
+                                <li>
+                                    <?= Html::a('Picking Trouble by Vendor <span class="japanesse-word">(ベンダー別のピッキング問題）</span>', ['parts-uncountable-monthly-report']); ?>
                                 </li>
                             </ol>
                         </div>
