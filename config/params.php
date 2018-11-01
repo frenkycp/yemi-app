97c5102fae0eec1a2113a023cc04b82771a0fd2d
FRENKY-GLORIA\Frenky - Gloria <friendq.c.p@gmail.com>
2018-11-01 15:59:04 +0700

    - add params

--- config/params.php
+++ config/params.php
@@ -3,5 +3,16 @@
 return [
     'adminEmail' => 'admin@example.com',
     'japanesse_update_chart' => 'Update Chart <span class="japanesse">(更新)',
-    'update_chart_btn' => '<button type="submit" class="btn btn-success">Update Chart <span class="japanesse">(更新)</button>'
+    'update_chart_btn' => '<button type="submit" class="btn btn-success">Update Chart <span class="japanesse">(更新)</button>',
+    'year_arr' => getYearArray(),
 ];
+
+function getYearArray()
+{
+	$start_year = 2018;
+	$end_year = ((int)date('Y') + 1);
+	for ($i = $start_year; $i <= $end_year; $i++) { 
+		$year_arr[$i] = $i;
+	}
+	return $year_arr;
+}
