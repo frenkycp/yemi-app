d6bfa073b420e6ad11c6fe0de0cb6bc12c980322
FRENKY-GLORIA\Frenky - Gloria <friendq.c.p@gmail.com>
2019-02-01 09:55:34 +0700

    - set default plan date to today

--- controllers/ProdPlanDataController.php
+++ controllers/ProdPlanDataController.php
@@ -2,6 +2,10 @@
 
 namespace app\controllers;
 
+use dmstr\bootstrap\Tabs;
+use yii\helpers\Url;
+use app\models\search\ProdPlanDataSearch;
+
 /**
 * This is the class for controller "ProdPlanDataController".
 */
@@ -12,4 +16,31 @@ class ProdPlanDataController extends \app\controllers\base\ProdPlanDataControlle
         //apply role_action table for privilege (doesn't apply to super admin)
         return \app\models\Action::getAccess($this->id);
     }
+
+    /**
+	* Lists all WipEffTbl models.
+	* @return mixed
+	*/
+	public function actionIndex()
+	{
+	    $searchModel  = new ProdPlanDataSearch;
+
+	    $searchModel->plan_date = date('Y-m-d');
+	    if(\Yii::$app->request->get('plan_date') !== null)
+	    {
+	    	$searchModel->plan_date = \Yii::$app->request->get('plan_date');
+	    }
+
+	    $dataProvider = $searchModel->search($_GET);
+
+		Tabs::clearLocalStorage();
+
+		Url::remember();
+		\Yii::$app->session['__crudReturnUrl'] = null;
+
+		return $this->render('index', [
+			'dataProvider' => $dataProvider,
+		    'searchModel' => $searchModel,
+		]);
+	}
 }
