e043de75a0cee1eba58b2d8727ca68fb4fc38fc3
frenkycp98 <friendq.c.p@gmail.com>
2019-04-08 07:29:37 +0700

    - add link

--- views/site/index2.php
+++ views/site/index2.php
@@ -99,6 +99,9 @@ $this->registerCss("
                     <div class="list-group">
                         <a class="list-group-item" target="_blank" href="http://172.17.144.217/kaizen"><i class="fa fa-fw fa-tag"></i> E-Kaizen<span class="japanesse-word"></span></a>
                     </div>
+                    <div class="list-group">
+                        <a class="list-group-item" target="_blank" href="http://172.17.144.6:99/plus/index_prd/index_prd_efficiency.php"><i class="fa fa-fw fa-tag"></i> Line Efficiency (MITA+)<span class="japanesse-word"></span></a>
+                    </div>
                 </div>
     		</div>
     		<div class="col-sm-3">
