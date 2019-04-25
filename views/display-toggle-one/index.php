c06b63a5f82a440be1629925d0a46332fc6a4fbf
frenkycp98 <friendq.c.p@gmail.com>
2019-04-25 08:52:35 +0700

    - add display toggle

---
+++ views/display-toggle-one/index.php
@@ -0,0 +1,38 @@
+<?php
+use yii\web\View;
+
+$this->title = Yii::t('app', 'Display Toggle 1');
+$this->params['breadcrumbs'][] = $this->title;
+
+date_default_timezone_set('Asia/Jakarta');
+
+$script = "
+    window.onload = setupRefresh;
+
+    function setupRefresh() {
+        window.setInterval(refreshPage, 10000);
+    }
+    function refreshPage() {
+        var src = $('#frame1').attr('src');
+        var link_arr = [
+            'http://172.17.144.6:99/plus/index_prd/index_prd_efficiency.php',
+            'http://localhost/yemi-app/web/finish-good-stock-display',
+        ];
+        var index = link_arr.indexOf(src);
+        var arr_length = link_arr.length;
+        index++;
+        if(index == arr_length){
+            index = 0;
+        }
+        var new_src = link_arr[index];
+
+        $('#frame1').attr('src',new_src);
+        //alert(new_src);
+    }
+";
+$this->registerJs($script, View::POS_HEAD );
+?>
+
+<iframe id="frame1" src="http://172.17.144.6:99/plus/index_prd/index_prd_efficiency.php" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
+    Your browser doesn't support iframes
+</iframe>
