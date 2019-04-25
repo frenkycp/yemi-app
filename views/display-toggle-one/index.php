a2ecd356a7dec5ba51ca2fe0d70b4f6c479f1ba2
frenkycp98 <friendq.c.p@gmail.com>
2019-04-25 10:08:04 +0700

    - update display toggle one

--- views/display-toggle-one/index.php
+++ views/display-toggle-one/index.php
@@ -10,13 +10,13 @@ $script = "
     window.onload = setupRefresh;
 
     function setupRefresh() {
-        window.setInterval(refreshPage, 10000);
+        window.setInterval(refreshPage, 15000);
     }
     function refreshPage() {
         var src = $('#frame1').attr('src');
         var link_arr = [
-            'http://172.17.144.6:99/plus/index_prd/index_prd_efficiency.php',
-            'http://localhost/yemi-app/web/finish-good-stock-display',
+            'http://172.17.144.6:86/daily-container-display',
+            'http://172.17.144.6:86/finish-good-stock-display',
         ];
         var index = link_arr.indexOf(src);
         var arr_length = link_arr.length;
@@ -33,6 +33,6 @@ $script = "
 $this->registerJs($script, View::POS_HEAD );
 ?>
 
-<iframe id="frame1" src="http://172.17.144.6:99/plus/index_prd/index_prd_efficiency.php" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
+<iframe id="frame1" src="http://172.17.144.6:86/daily-container-display" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
     Your browser doesn't support iframes
 </iframe>
