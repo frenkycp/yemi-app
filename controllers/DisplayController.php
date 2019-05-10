d71583a85495b059d0e3526f51770de3a907ce40
frenkycp98 <friendq.c.p@gmail.com>
2019-05-10 11:29:53 +0700

    - add display for parts-picking-status

--- controllers/DisplayController.php
+++ controllers/DisplayController.php
@@ -3,15 +3,21 @@ namespace app\controllers;
 
 use yii\web\Controller;
 use yii\helpers\Url;
+use yii\helpers\ArrayHelper;
 
 //production-monthly-inspection
 use app\models\InspectionReportView;
 use app\models\SernoInput;
 use app\models\PlanReceivingPeriod;
 
+//parts-picking-status
+use app\models\PickingLocation;
+use app\models\VisualPickingView02;
+use app\models\VisualPickingView;
+
 class DisplayController extends Controller
 {
-	public function actionProductionMonthlyInspection($value='')
+	public function actionProductionMonthlyInspection()
 	{
 		$this->layout = 'clean';
 		$categories = [];
@@ -105,4 +111,202 @@ class DisplayController extends Controller
     		''
     	]);
 	}
+
+	public function actionPartsPickingStatus()
+	{
+		$this->layout = 'clean';
+		date_default_timezone_set('Asia/Jakarta');
+        set_time_limit(500);
+    	$data = [];
+    	$model = new PickingLocation();
+    	$model->location = 'WF01';
+    	$model->load($_POST);
+    	$dropdown_loc = ArrayHelper::map(VisualPickingView02::find()->select('analyst, analyst_desc')->groupBy('analyst, analyst_desc')->orderBy('analyst_desc ASC')->all(), 'analyst', 'analyst_desc');
+
+    	$visual_picking_arr = VisualPickingView02::find()
+    	->where([
+    		'year' => date('Y'),
+    		'analyst' => $model->location
+    	])
+    	->orderBy('week ASC, req_date')
+    	->all();
+
+    	$week_period = $this->getWeekPeriod($model->location);
+    	$today = new \DateTime(date('Y-m-d'));
+		$this_week = $today->format("W");
+		if (!in_array($this_week, $week_period)) {
+			$this_week = end($week_period);
+		}
+
+    	foreach ($week_period as $week_no) {
+    		$tmp_category = [];
+    		$tmp_data_open = [];
+    		$tmp_data_close = [];
+
+            $tmp_data_ordered = [];
+            $tmp_data_started = [];
+            $tmp_data_completed = [];
+            $tmp_data_handover = [];
+    		foreach ($visual_picking_arr as $visual_picking) {
+    			if ($week_no == $visual_picking->week) {
+    				$tmp_category[] = date('Y-m-d', strtotime($visual_picking->req_date));
+
+    				$open_qty = $visual_picking->slip_open;
+    				$close_qty = $visual_picking->slip_close;
+
+                    $ordered_qty = $visual_picking->total_ordered;
+                    $started_qty = $visual_picking->total_started;
+                    $completed_qty = $visual_picking->total_completed;
+                    $handover_qty = $visual_picking->slip_close;
+    				$total_qty = $visual_picking->slip_count;
+
+    				$open_percentage = 0;
+    				$close_percentage = 0;
+
+                    $ordered_percentage = 0;
+                    $started_percentage = 0;
+                    $completed_percentage = 0;
+                    $handover_percentage = 0;
+    				if ($total_qty > 0) {
+    					$open_percentage = round((($open_qty / $total_qty) * 100), 2);
+    					$close_percentage = round((($close_qty / $total_qty) * 100), 2);
+
+                        $ordered_percentage = round((($ordered_qty / $total_qty) * 100), 2);
+                        $started_percentage = round((($started_qty / $total_qty) * 100), 2);
+                        $completed_percentage = round((($completed_qty / $total_qty) * 100), 2);
+                        $handover_percentage = round((($handover_qty / $total_qty) * 100), 2);
+    				}
+
+                    $tmp_data_ordered[] = [
+                        'y' => $ordered_percentage == 0 ? null : $ordered_percentage,
+                        'remark' => $this->partsPickingGetRemark($visual_picking->req_date, $visual_picking->analyst, [1, 2], 'O')
+                    ];
+                    $tmp_data_started[] = [
+                        'y' => $started_percentage == 0 ? null : $started_percentage,
+                        'remark' => $this->partsPickingGetRemark($visual_picking->req_date, $visual_picking->analyst, 3, 'O')
+                    ];
+                    $tmp_data_completed[] = [
+                        'y' => $completed_percentage == 0 ? null : $completed_percentage,
+                        'remark' => $this->partsPickingGetRemark($visual_picking->req_date, $visual_picking->analyst, [4, 5], 'O')
+                    ];
+                    $tmp_data_handover[] = [
+                        'y' => $handover_percentage == 0 ? null : $handover_percentage,
+                        'remark' => $this->partsPickingGetRemark($visual_picking->req_date, $visual_picking->analyst, 5, 'C')
+                    ];
+
+    			}
+    		}
+    		$data[$week_no][] = [
+    			'category' => $tmp_category,
+                'data' => [
+                    [
+                        'name' => 'ORDERED （受注）',
+                        'data' => $tmp_data_ordered,
+                        'color' => 'rgba(200, 200, 200, 0.5)',
+                    ],
+                    [
+                        'name' => 'STARTED (ピッキング中)',
+                        'data' => $tmp_data_started,
+                        'color' => 'rgba(240, 240, 0, 0.5)',
+                    ],
+                    [
+                        'name' => 'COMPLETED (ピッキング完了)',
+                        'data' => $tmp_data_completed,
+                        'color' => 'rgba(0, 150, 255, 0.5)',
+                    ],
+                    [
+                        'name' => 'HANDOVER （後工程に引渡し）',
+                        'data' => $tmp_data_handover,
+                        'color' => 'rgba(0, 240, 0, 0.5)',
+                    ],
+                ],
+    		];
+    	}
+
+    	return $this->render('parts-picking-status', [
+    		'model' => $model,
+    		'dropdown_loc' => $dropdown_loc,
+    		'data' => $data,
+    		'this_week' => $this_week
+    	]);
+	}
+
+	public function partsPickingGetRemark($req_date, $analyst, $stage_id, $stat)
+    {
+    	$data_arr = VisualPickingView::find()
+    	->where([
+    		'req_date' => $req_date,
+    		'analyst' => $analyst,
+            'stage_id' => $stage_id,
+            'stat' => $stat,
+    	])
+    	->orderBy('set_list_no ASC')
+    	->all();
+
+    	$data = '<table class="table table-bordered table-striped table-hover">';
+		$data .= 
+		'<tr>
+			<th class="text-center">Setlist No</th>
+			<th class="text-center">Parent</th>
+			<th>Parent Description</th>
+			<th class="text-center" style="width: 100px;">Req Date</th>
+			<th class="text-center">Plan Qty</th>
+			<th class="text-center">Part Count</th>
+			<th class="text-center">Man Power</th>
+            <th class="text-center" style="width:90px;">Start Date</th>
+            <th class="text-center" style="width:90px;">Completed Date</th>
+			<th class="text-center">Confirm</th>
+            <th class="text-center">PTS Note</th>
+		</tr>'
+		;
+
+		foreach ($data_arr as $value) {
+            $req_date = $value['req_date'] == null ? '-' : date('Y-m-d', strtotime($value['req_date']));
+            $start_date = $value['start_date'] == null ? '-' : date('Y-m-d H:i:s', strtotime($value['start_date']));
+            $completed_date = $value['completed_date'] == null ? '-' : date('Y-m-d H:i:s', strtotime($value['completed_date']));
+			$data .= '
+				<tr>
+					<td class="text-center">' . $value['set_list_no'] . '</td>
+					<td class="text-center">' . $value['parent'] . '</td>
+					<td>' . $value['parent_desc'] . '</td>
+					<td class="text-center">' . $req_date . '</td>
+					<td class="text-center">' . $value['plan_qty'] . '</td>
+					<td class="text-center">' . $value['part_count'] . '</td>
+					<td class="text-center">' . $value['man_power'] . '</td>
+                    <td class="text-center">' . $start_date . '</td>
+                    <td class="text-center">' . $completed_date . '</td>
+					<td class="text-center">' . $value['stage_desc'] . '</td>
+                    <td>' . $value['pts_note'] . '</td>
+				</tr>
+				';
+		}
+
+		$data .= '</table>';
+		return $data;
+    }
+
+	public function getWeekPeriod($analyst)
+    {
+    	$return_arr = [];
+    	$data_arr = VisualPickingView02::find()
+    	->select('week')
+    	->where([
+    		'year' => date('Y'),
+    		'analyst' => $analyst
+    	])
+    	->groupBy('week')
+    	->all();
+
+    	$selisih = count($data_arr) - 10;
+
+    	$i = 1;
+    	foreach ($data_arr as $value) {
+    		if ($i > $selisih) {
+    			$return_arr[] = $value->week;
+    		}
+    		$i++;
+    	}
+
+    	return $return_arr;
+    }
 }
---
+++ views/display/parts-picking-status.php
@@ -0,0 +1,211 @@
+<?php
+use miloschuman\highcharts\Highcharts;
+use yii\web\JsExpression;
+use yii\web\View;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\bootstrap\ActiveForm;
+
+$this->title = [
+    'page_title' => 'Weekly Picking Status <span class="japanesse text-green">(週次ピッキング状況)</span> - Handover based <span class="japanesse text-green">(使用工程との引渡し基準）</span>',
+    'tab_title' => 'Weekly Picking Status',
+    'breadcrumbs_title' => 'Weekly Picking Status'
+];
+
+$this->registerCss("
+    .japanesse { font-family: 'MS PGothic', Osaka, Arial, sans-serif; color: #82b964;}
+    .form-control, .control-label {background-color: #33383D; color: white; border-color: white;}
+    .form-control {font-size: 20px; height: 40px;}
+    .content-header {color: white;}
+    //.box-body {background-color: #33383D;}
+    .box-title {font-weight: bold;}
+    .box-header .box-title, .control-label{font-size: 2em;}
+    .container {width: auto;}
+    .content-header>h1 {font-size: 3.5em; font-family: sans-serif; font-weight: bold;}
+    body {background-color: #ecf0f5;}
+    .form-group {margin-bottom: 0px;}
+    body, .content-wrapper {background-color: #33383D;}
+    .small-box .icon {top: 1px;}
+    .inner p {font-size: 18px;}
+    .form-horizontal .control-label {padding-top: 0px;}
+    .active a {background-color: #3c8dbc !important; font-size: 18px; color: white !important;}
+");
+
+date_default_timezone_set('Asia/Jakarta');
+
+$script = <<< JS
+    window.onload = setupRefresh;
+
+    function setupRefresh() {
+      setTimeout("refreshPage();", 300000); // milliseconds
+    }
+    function refreshPage() {
+       window.location = location.href;
+    }
+JS;
+$this->registerJs($script, View::POS_HEAD );
+
+/*echo '<pre>';
+print_r($series);
+echo '</pre>';*/
+
+?>
+<?php $form = ActiveForm::begin([
+    'id' => 'form_index',
+    'layout' => 'horizontal',
+    'enableClientValidation' => true,
+    'errorSummaryCssClass' => 'error-summary alert alert-danger',
+    'fieldConfig' => [
+             'template' => "{label}\n{beginWrapper}\n{input}\n{hint}\n{error}\n{endWrapper}",
+             'horizontalCssClasses' => [
+                 'label' => 'col-sm-5',
+                 #'offset' => 'col-sm-offset-4',
+                 'wrapper' => 'col-sm-7',
+                 'error' => '',
+                 'hint' => '',
+             ],
+         ],
+    ]
+    );
+?>
+
+<div class="row">
+    <div class="col-md-3">
+        <?= $form->field($model, 'location')->dropDownList($dropdown_loc); ?>
+    </div>
+    <button type="submit" class="btn btn-success btn-lg">Update Chart <span class="japanesse" style="color: white;">(更新)</button>
+</div>
+
+<?php ActiveForm::end(); ?>
+<h4 class="box-title"><i class="fa fa-tag"></i> Last Update : <?= date('Y-m-d H:i:s') ?></h4>
+<div class="nav-tabs-custom">
+    <ul class="nav nav-tabs">
+        <?php
+        foreach ($data as $key => $value) {
+            if($key == $this_week)
+            {
+                echo '<li class="active"><a href="#tab_1_' . $key . '" data-toggle="tab">Week ' . $key . '</a></li>';
+            }
+            else
+            {
+                echo '<li><a href="#tab_1_' . $key . '" data-toggle="tab">Week ' . $key . '</a></li>';
+            }
+        }
+        ?>
+        <li class="pull-right" style="display: <?= Yii::$app->user->identity->role->name == 'Picking List Editor' ? '' : 'none'; ?>"><?= Html::a('<i class="fa fa-gear"></i>', ['visual-picking-list/index']); ?></li>
+    </ul>
+    <div class="tab-content">
+        <hr>
+        <?php
+        foreach ($data as $key => $value) {
+            if($key == $this_week)
+            {
+                echo '<div class="tab-pane active" id="tab_1_' . $key .'">';
+            }
+            else
+            {
+                echo '<div class="tab-pane" id="tab_1_' . $key .'">';
+            }
+
+            echo Highcharts::widget([
+                'scripts' => [
+                    //'modules/exporting',
+                    'themes/grid-light',
+                ],
+                'options' => [
+                    'chart' => [
+                        'type' => 'column',
+                        'height' => 500,
+                        'style' => [
+                            'fontFamily' => 'Source Sans Pro'
+                        ],
+                    ],
+                    'credits' => [
+                        'enabled' =>false
+                    ],
+                    'title' => [
+                        'text' => ''
+                    ],
+                    'subtitle' => [
+                        'text' => ''
+                    ],
+                    'xAxis' => [
+                        'type' => 'category',
+                        'categories' => $value[0]['category'],
+                        'labels' => [
+                            'style' => [
+                                'fontSize' => '18px',
+                                'fontWeight' => 'bold'
+                            ],
+                        ],
+                    ],
+                    'yAxis' => [
+                        'min' => 0,
+                        'title' => [
+                            'text' => 'Total Completion'
+                        ],
+                        //'gridLineWidth' => 0,
+                    ],
+                    'legend' => [
+                        'enabled' => true,
+                        'itemStyle' => [
+                            'fontSize' => '26px',
+                        ],
+                    ],
+                    'tooltip' => [
+                        'enabled' => false,
+                        //'formatter' => new JsExpression('function(){ return "Percentage : " + this.y + "%<br/>" + "Qty : " + this.point.qty + " pcs"; }'),
+                    ],
+                    'plotOptions' => [
+                        'column' => [
+                            'stacking' => 'percent',
+                            'dataLabels' => [
+                                'enabled' => true,
+                                'format' => '{point.percentage:.2f}%',
+                                'color' => 'black',
+                                //'formatter' => new JsExpression('function(){ if(this.y != 0) { return this.y; } }'),
+                                'style' => [
+                                    'fontSize' => '18px',
+                                    'textOutline' => '0px',
+                                    'fontWeight' => '0'
+                                ],
+                            ],
+                        ],
+                        'series' => [
+                            'cursor' => 'pointer',
+                            'point' => [
+                                'events' => [
+                                    'click' => new JsExpression('
+                                        function(){
+                                            $("#modal").modal("show").find(".modal-body").html(this.options.remark);
+                                        }
+                                    '),
+                                ]
+                            ]
+                        ]
+                    ],
+                    'series' => $value[0]['data']
+                ],
+            ]);
+
+            echo '</div>';
+        }
+
+        yii\bootstrap\Modal::begin([
+            'id' =>'modal',
+            'header' => '<h3>Detail Information</h3>',
+            'size' => 'modal-lg',
+        ]);
+        yii\bootstrap\Modal::end();
+        ?>
+    </div>
+</div>
+<h4 class="text-center bg-navy" style="padding: 5px; display: none;">
+    <span>ORDERED</span>
+    <i class="fa fa-fw fa-long-arrow-right"></i>
+    <span>STARTED</span>
+    <i class="fa fa-fw fa-long-arrow-right"></i>
+    <span>COMPLETED</span>
+    <i class="fa fa-fw fa-long-arrow-right"></i>
+    <span class="text-green">HAND OVER</span>
+</h4>
