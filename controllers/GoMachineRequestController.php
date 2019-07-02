23fcdac8f1194ec296ad9095638eea072fa8501c
frenkycp98 <friendq.c.p@gmail.com>
2019-07-02 13:38:25 +0700

    - minor change

--- controllers/GoMachineRequestController.php
+++ controllers/GoMachineRequestController.php
@@ -13,7 +13,7 @@ class GoMachineRequestController extends Controller
         //apply role_action table for privilege (doesn't apply to super admin)
         return \app\models\Action::getAccess($this->id);
     }
-    
+
 	public function actionIndex()
 	{
 		date_default_timezone_set('Asia/Jakarta');
@@ -29,50 +29,48 @@ class GoMachineRequestController extends Controller
 			$model->request_date = \Yii::$app->request->get('request_date');
 		}
 
-		if ($model->load($_GET)) {
-			$tmp_order_arr = GojekOrderView01::find()
-			->select([
-				'request_ymd', 'request_hour',
-				'qty_open' => 'SUM(CASE WHEN STAT = \'O\' THEN 1 ELSE 0 END)',
-				'qty_close' => 'SUM(CASE WHEN STAT = \'C\' THEN 1 ELSE 0 END)',
-			])
-			->where([
-				'source' => 'MCH',
-				'request_ymd' => $model->request_date
-			])
-			->groupBy('request_ymd, request_hour')
-			->orderBy('request_hour')
-			->all();
-			//echo $model->request_date;
-			//echo count($tmp_order_arr);
+		$tmp_order_arr = GojekOrderView01::find()
+		->select([
+			'request_ymd', 'request_hour',
+			'qty_open' => 'SUM(CASE WHEN STAT = \'O\' THEN 1 ELSE 0 END)',
+			'qty_close' => 'SUM(CASE WHEN STAT = \'C\' THEN 1 ELSE 0 END)',
+		])
+		->where([
+			'source' => 'MCH',
+			'request_ymd' => $model->request_date
+		])
+		->groupBy('request_ymd, request_hour')
+		->orderBy('request_hour')
+		->all();
+		//echo $model->request_date;
+		//echo count($tmp_order_arr);
 
-			$tmp_data = [];
-			foreach ($tmp_order_arr as $key => $value) {
-				$tmp_request_date = (strtotime($value->request_ymd . ' ' . $value->request_hour . " +7 hours") * 1000);
-				if ($value->qty_open > 0) {
-					$tmp_data['open'][] = [
-						'x' => $tmp_request_date,
-						'y' => (int)$value->qty_open,
-						'url' => Url::to(['get-remark', 'request_ymd' => $value->request_ymd, 'request_hour' => $value->request_hour, 'STAT' => 'O']),
-					];
-				}
-				
-				if ($value->qty_close > 0) {
-					$tmp_data['close'][] = [
-						'x' => $tmp_request_date,
-						'y' => (int)$value->qty_close,
-						'url' => Url::to(['get-remark', 'request_ymd' => $value->request_ymd, 'request_hour' => $value->request_hour, 'STAT' => 'C']),
-					];
-				}
-				
+		$tmp_data = [];
+		foreach ($tmp_order_arr as $key => $value) {
+			$tmp_request_date = (strtotime($value->request_ymd . ' ' . $value->request_hour . " +7 hours") * 1000);
+			if ($value->qty_open > 0) {
+				$tmp_data['open'][] = [
+					'x' => $tmp_request_date,
+					'y' => (int)$value->qty_open,
+					'url' => Url::to(['get-remark', 'request_ymd' => $value->request_ymd, 'request_hour' => $value->request_hour, 'STAT' => 'O']),
+				];
 			}
-
-			$data[] = [
-				'name' => 'ORDER (OPEN)',
-				'data' => $tmp_data['open'],
-				'color' => 'rgba(255, 0, 0, 0.7)',
-			];
+			
+			if ($value->qty_close > 0) {
+				$tmp_data['close'][] = [
+					'x' => $tmp_request_date,
+					'y' => (int)$value->qty_close,
+					'url' => Url::to(['get-remark', 'request_ymd' => $value->request_ymd, 'request_hour' => $value->request_hour, 'STAT' => 'C']),
+				];
+			}
+			
 		}
+
+		$data[] = [
+			'name' => 'ORDER (OPEN)',
+			'data' => $tmp_data['open'],
+			'color' => 'rgba(255, 0, 0, 0.7)',
+		];
 
 		return $this->render('index', [
 			'data' => $data,
@@ -90,7 +88,7 @@ class GoMachineRequestController extends Controller
     	}
 		$remark = '<div class="modal-header">
 			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-			<h3>Request ' . $status . '</h3>
+			<h3>Request ' . $status . ' <small>(' . $request_ymd . ')</small></h3>
 		</div>
 		<div class="modal-body">
 		';
@@ -119,7 +117,7 @@ class GoMachineRequestController extends Controller
 	    $no = 1;
 	    foreach ($data_arr as $key => $value) {
 	    	$remark .= '<tr style="font-size: 12px;">
-	    		<td class="text-center">' . date('Y-m-d H:i:s', strtotime($value->request_date)) . '</td>
+	    		<td class="text-center" style="font-weight: bold;">' . date('H:i', strtotime($value->request_date)) . '</td>
 	    		<td class="text-center">' . $value->slip_id . '</td>
 	    		<td>' . $value->to_loc . '</td>
 	    		<td>' . $value->item_desc . '</td>
