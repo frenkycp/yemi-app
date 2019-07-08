0264f8182ae4be9555796e230fc831d6573c85e6
frenkycp98 <friendq.c.p@gmail.com>
2019-07-08 15:56:36 +0700

    - crusher chart clickable

--- controllers/CrusherChartController.php
+++ controllers/CrusherChartController.php
@@ -4,14 +4,15 @@ namespace app\controllers;
 
 use yii\web\Controller;
 use app\models\CrusherTbl;
+use yii\helpers\Url;
 
 class CrusherChartController extends Controller
 {
-	public function behaviors()
+	/*public function behaviors()
     {
         //apply role_action table for privilege (doesn't apply to super admin)
         return \app\models\Action::getAccess($this->id);
-    }
+    }*/
     
 	public function actionIndex()
 	{
@@ -39,7 +40,8 @@ class CrusherChartController extends Controller
 				$proddate = (strtotime($value->date . " +7 hours") * 1000);
 				$tmp_data[$value->model][] = [
 					'x' => $proddate,
-					'y' => round($value->consume, 1)
+					'y' => round($value->consume, 1),
+					'url' => Url::to(['get-remark', 'input_date' => $value->date, 'model' => $value->model]),
 				];
 			}
 
@@ -55,5 +57,47 @@ class CrusherChartController extends Controller
 			'data' => $data,
 			'model' => $model,
 		]);
+	}
+	
+	public function actionGetRemark($input_date, $model)
+	{
+		$tmp_crusher = CrusherTbl::find()
+		->where([
+			'date' => $input_date,
+			'model' => $model
+		])
+		->all();
+		$remark = '<div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+			<h3>' . $model . ' <small>(' . $input_date . ')</small></h3>
+		</div>
+		<div class="modal-body">
+		';
+		
+	    $remark .= '<table class="table table-bordered table-striped table-hover">';
+	    $remark .= '<tr style="font-size: 16px;">
+	    	<th class="text-center">No.</th>
+	    	<th class="text-center">Part</th>
+	    	<th class="text-center">Qty</th>
+	    	<th class="text-center">BOM</th>
+	    	<th class="text-center">Consume (Kg)</th>
+	    </tr>';
+
+	    $no = 1;
+	    foreach ($tmp_crusher as $key => $value) {
+	    	$remark .= '<tr style="font-size: 16px;">
+	    		<td class="text-center">' . $no . '</td>
+	    		<td class="text-center">' . $value->part . '</td>
+	    		<td class="text-center">' . $value->qty . '</td>
+	    		<td class="text-center">' . $value->bom . '</td>
+	    		<td class="text-center">' . $value->consume . '</td>
+	    	</tr>';
+	    	$no++;
+	    }
+
+	    $remark .= '</table>';
+	    $remark .= '</div>';
+
+	    return $remark;
 	}
 }
--- views/crusher-chart/index.php
+++ views/crusher-chart/index.php
@@ -136,6 +136,19 @@ echo '</pre>';*/
                                 //'format' => '{point.percentage:.1f}%',
                             ],
                         ],
+                        'series' => [
+                            'cursor' => 'pointer',
+                            'point' => [
+                                'events' => [
+                                    'click' => new JsExpression("
+                                        function(e){
+                                            e.preventDefault();
+                                            $('#modal').modal('show').find('.modal-content').html('<div class=\"text-center\">" . Html::img('@web/loading-01.gif', ['alt'=>'some', 'class'=>'thing']) . "</div>').load(this.options.url);
+                                        }
+                                    "),
+                                ]
+                            ]
+                        ]
                     ],
                     'series' => $data
                 ],
@@ -144,3 +157,12 @@ echo '</pre>';*/
         </div>
     </div>
 </div>
+
+<?php
+yii\bootstrap\Modal::begin([
+    'id' =>'modal',
+    'header' => '<h3>Detail Information</h3>',
+    'size' => 'modal-lg',
+]);
+yii\bootstrap\Modal::end();
+?>
