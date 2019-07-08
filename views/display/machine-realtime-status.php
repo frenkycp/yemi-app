47d9a36c26963b3e1c863dffb132acfcc460d0b8
frenkycp98 <friendq.c.p@gmail.com>
2019-07-08 10:54:30 +0700

    - display for machine realtime

---
+++ views/display/machine-realtime-status.php
@@ -0,0 +1,157 @@
+<?php
+use miloschuman\highcharts\Highcharts;
+use yii\web\JsExpression;
+use yii\web\View;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\helpers\ArrayHelper;
+use yii\bootstrap\ActiveForm;
+use kartik\date\DatePicker;
+use kartik\select2\Select2;
+
+$this->title = [
+    'page_title' => null,
+    'tab_title' => 'Machine Daily Status',
+    'breadcrumbs_title' => 'Machine Daily Status'
+];
+//$this->params['breadcrumbs'][] = $this->title['breadcrumbs_title'];
+$color = 'ForestGreen';
+
+$this->registerCss("
+    .japanesse { font-family: 'MS PGothic', Osaka, Arial, sans-serif; color: #82b964;}
+    .form-control, .control-label {background-color: #33383D; color: white; border-color: white;}
+    //.form-control {font-size: 20px; height: 40px;}
+    .content-header {color: white;}
+    //.box-body {background-color: #33383D;}
+    .box-title {font-weight: bold;}
+    .box-header .box-title{font-size: 2em;}
+    .container {width: auto;}
+    .content-header>h1 {font-size: 3.5em; font-family: sans-serif; font-weight: bold;}
+    body {background-color: #ecf0f5;}
+    .form-group {margin-bottom: 0px;}
+    body, .content-wrapper {background-color: #33383D;}
+    .small-box .icon {top: 1px;}
+    .inner p {font-size: 18px;}
+    .form-horizontal .control-label {padding-top: 0px;}
+
+    #progress-tbl{
+        //border:1px solid #29B6F6;
+        border-top: 0;
+    }
+    #progress-tbl > thead > tr > th{
+        border:1px solid #8b8c8d;
+        background-color: #595F66;
+        color: white;
+        font-size: 24px;
+        border-bottom: 7px solid #ddd;
+        vertical-align: middle;
+    }
+    #progress-tbl > tbody > tr > td{
+        border:1px solid #777474;
+        font-size: 24px;
+        //background-color: #B3E5FC;
+        //font-weight: 1000;
+        color: #FFF;
+        vertical-align: middle;
+    }
+");
+
+date_default_timezone_set('Asia/Jakarta');
+
+$script = "
+    window.onload = setupRefresh;
+
+    function setupRefresh() {
+      setTimeout(\"refreshPage();\", 60000); // milliseconds
+    }
+    function refreshPage() {
+       window.location = location.href;
+    }
+
+    function update_data(){
+        $.ajax({
+            type: 'POST',
+            url: '" . Url::to(['current-status']) . "',
+            success: function(data){
+                //alert(data[0].mesin_id);
+                var content = '';
+                $.each(data , function(index, val) {
+                    var background_color = 'white';
+                    var color = 'black';
+                    var status_txt = 'IDLE';
+                    if(val.status_warna == 'KUNING'){
+                        //background_color = 'yellow';
+                        color = 'black';
+                        background_color = 'yellow';
+                        status_txt = 'HADNLING';
+                    } else if (val.status_warna == 'HIJAU') {
+                        background_color = 'green';
+                        status_txt = 'RUNNING';
+                        color = 'white';
+                    } else if (val.status_warna == 'BIRU') {
+                        background_color = 'blue';
+                        status_txt = 'SETTING';
+                        color = 'white';
+                    } else if (val.status_warna == 'MERAH') {
+                        background_color = 'red';
+                        status_txt = 'STOP';
+                        color = 'white';
+                    }
+
+                    $('#current-'+val.mesin_id).html('<button style=\"background-color: ' + background_color + '; color: ' + color + '; font-size: 16px;\" type=\"button\" class=\"btn btn-block btn-default\">' + status_txt + '</button>');
+
+                    /*if (background_color != 'white') {
+                        $('#'+val.mesin_id).css('background-color', background_color);
+                        $('#'+val.mesin_id).css('color', color);
+                    }*/
+                    //var label = val.mesin_id+' - '+val.mesin_description;
+                    //var tmp = '<div class=\"row\"><div class=\"col-md-12\"><button type=\"button\" class=\"\"></button></div></div>';
+                    //console.log(index, val.mesin_id);
+                });
+            },
+            complete: function(){
+                setTimeout(function(){update_data();}, 1000);
+            }
+        });
+    }
+
+    $(document).ready(function() {
+        update_data();
+    });
+";
+$this->registerJs($script, View::POS_HEAD );
+
+/*echo '<pre>';
+print_r($tmp_data);
+echo '</pre>';*/
+//echo Yii::$app->request->baseUrl;
+?>
+<table class="table" id="progress-tbl">
+    <thead>
+        <tr>
+            <th style="width: 40%;">Machine</th>
+            <th style="width: 10%;">Current</th>
+            <th style="width: 30%;">Operational Status</th>
+            <th style="width: 20%;">Summary</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($machine_arr as $key => $value) {
+            ?>
+            <tr>
+                <td><?= $value; ?></td>
+                <td id="current-<?= $key; ?>"></td>
+                <td></td>
+                <td>
+                    <span class="label bg-green" style="font-weight: normal; font-size: 14px;">RUNNING : X%</span>
+                    <span class="label bg-blue" style="font-weight: normal; font-size: 14px;">SETTING : X%</span>
+                    <span class="label bg-yellow" style="font-weight: normal; font-size: 14px;">HANDLING : X%</span>
+                    <span class="label bg-red" style="font-weight: normal; font-size: 14px;">STOP : X%</span>
+                    <span class="label bg-gray" style="font-weight: normal; font-size: 14px;">IDLING : X%</span>
+                </td>
+            </tr>
+        <?php }
+        ?>
+    </tbody>
+</table>
