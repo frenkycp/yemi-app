4af7907c44da9ae072864858dc293ea9d1bc4257
frenkycp98 <friendq.c.p@gmail.com>
2019-07-22 16:23:50 +0700

    - add go sub assy (80%)

---
+++ config/db_iot.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'sqlsrv:Server=174.17.144.109;Database=IOT',
+    'username' => '200074',
+    'password' => 'Fredy@yemi',
+    /*'dsn' => 'sqlsrv:Server=FRENKY-GLORIA\SQLEXPRESS;Database=PROD',
+    'username' => 'sa',
+    'password' => 'fg0908',*/
+    //'dsn' => 'dblib:host=localhost;Database=TEST',
+    //'username' => 'sa',
+    //'password' => 'f12345',
+    'charset' => 'utf8',
+    //'tablePrefix' => 'db_owner.',
+];
