8f92c6ddabb370c0a6132aa73dd7d2d151928f70
frenkycp98 <friendq.c.p@gmail.com>
2019-08-15 11:09:51 +0700

    - fix japanesse text

--- controllers/DisplayController.php
+++ controllers/DisplayController.php
@@ -115,7 +115,7 @@ class DisplayController extends Controller
                 $meeting_content .= '<tr style="color: rgba(255, 235, 59, 1); opacity: ' . $opacity . '; background-color: ' . $background_color . ';">
                 <td style="border-top: 0px; width: 540px; color: rgba(59, 255, 248, 1); font-size: 5.5em; padding: 12px 0px 0px 20px; letter-spacing: 2px;">(' . substr($value->jam_start, 0, 5) . '-' . substr($value->jam_end, 0, 5) .
                 ')</td>
-                <td style="border-top: 0px; font-size: 7em;  letter-spacing: 2px;">' . strtoupper($value->name) . '</td></tr>';
+                <td style="border-top: 0px; font-size: 7em;  letter-spacing: 2px;">' . htmlspecialchars(strtoupper($value->name)) . '</td></tr>';
             }
             $meeting_content .= '</table>';
         }
