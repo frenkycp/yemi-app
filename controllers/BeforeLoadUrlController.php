13a01519449326db377ea8cb5a890aeb252dc61d
frenkycp98 <friendq.c.p@gmail.com>
2019-08-21 16:08:29 +0700

    - add dept correction table

--- controllers/BeforeLoadUrlController.php
+++ controllers/BeforeLoadUrlController.php
@@ -5,19 +5,36 @@ use yii\web\Controller;
 use yii\helpers\Url;
 use app\models\Visitor;
 use app\models\KlinikInput;
+use app\models\DeptCorrection;
 
 class BeforeLoadUrlController extends Controller
 {
 	public function actionVisitorConfirm($url)
 	{
-		$tmp_visitor = Visitor::updateAll(['emp_dept' => 'PURCHASING'], ['LOWER(emp_dept)' => 'procurement']);
+		$correction_data = DeptCorrection::find()
+		->where([
+			'flag' => 1
+		])
+		->all();
 
+		foreach ($correction_data as $key => $value) {
+			Visitor::updateAll(['emp_dept' => $value->after], ['LOWER(emp_dept)' => $value->before]);
+		}
+		
 		return $this->redirect($url);
 	}
 
 	public function actionClinicConfirm($url)
 	{
-		$tmp_visitor = KlinikInput::updateAll(['dept' => 'PURCHASING'], ['LOWER(dept)' => 'procurement']);
+		$correction_data = DeptCorrection::find()
+		->where([
+			'flag' => 1
+		])
+		->all();
+
+		foreach ($correction_data as $key => $value) {
+			KlinikInput::updateAll(['dept' => $value->after], ['LOWER(dept)' => $value->before]);
+		}
 
 		return $this->redirect($url);
 	}
