9ee3c40a787da6fa7bf0f934edff044498dffc6b
frenkycp98 <friendq.c.p@gmail.com>
2019-09-23 07:33:53 +0700

    - update ProdAttendanceController

--- controllers/ProdAttendanceController.php
+++ controllers/ProdAttendanceController.php
@@ -20,82 +20,90 @@ class ProdAttendanceController extends Controller
         $model = new \yii\base\DynamicModel([
             'child_analyst', 'line', 'child_analyst_desc', 'nik'
         ]);
-        $model->addRule(['child_analyst_desc', 'nik'], 'string')
-        ->addRule(['line', 'child_analyst'], 'required');
+        $model->addRule(['child_analyst_desc', 'nik', 'child_analyst'], 'string')
+        ->addRule(['line'], 'number');
 
         $model->child_analyst = $loc;
         $model->line = $line;
 
         if($model->load(\Yii::$app->request->post())){
-            $find_data = ProdAttendanceData::find()
+        	$karyawan = Karyawan::find()
+            ->select('NIK, NAMA_KARYAWAN')
             ->where([
-                'nik' => $model->nik,
-                'child_analyst' => $model->child_analyst,
-                'line' => $model->line,
-                'posting_shift' => $posting_shift
+                'NIK' => $model->nik
             ])
             ->one();
-            $now = date('Y-m-d H:i:s');
 
-            if ($find_data->nik == null) {
-                $insert_attendance = new ProdAttendanceData;
-                $insert_attendance->period = date('Ym', strtotime($posting_shift));
-                $insert_attendance->posting_date = date('Y-m-d');
-                $insert_attendance->posting_shift = $posting_shift;
-                $insert_attendance->att_data_id = $model->child_analyst . '-' . $model->line . '-' . date('Ymd', strtotime($posting_shift)) . '-' . $model->nik;
-                $insert_attendance->nik = $model->nik;
+            if ($karyawan->NIK != null) {
+            	$find_data = ProdAttendanceData::find()
+	            ->where([
+	                'nik' => $model->nik,
+	                'child_analyst' => $model->child_analyst,
+	                'line' => $model->line,
+	                'posting_shift' => $posting_shift
+	            ])
+	            ->one();
+	            $now = date('Y-m-d H:i:s');
 
-                $karyawan = Karyawan::find()
-                ->select('NAMA_KARYAWAN')
-                ->where([
-                    'NIK' => $model->nik
-                ])
-                ->one();
-                $insert_attendance->name = $karyawan->NAMA_KARYAWAN;
-                $insert_attendance->check_in = $now;
-                $insert_attendance->child_analyst = $model->child_analyst;
+	            if ($find_data->nik == null) {
+	                $insert_attendance = new ProdAttendanceData;
+	                $insert_attendance->period = date('Ym', strtotime($posting_shift));
+	                $insert_attendance->posting_date = date('Y-m-d');
+	                $insert_attendance->posting_shift = $posting_shift;
+	                $insert_attendance->att_data_id = $model->child_analyst . '-' . $model->line . '-' . date('Ymd', strtotime($posting_shift)) . '-' . $model->nik;
+	                $insert_attendance->nik = $model->nik;
 
-                $location_data = WipLocation::find()
-                ->where([
-                    'child_analyst' => $model->child_analyst
-                ])
-                ->one();
-                $insert_attendance->child_analyst_desc = $location_data->child_analyst_desc;
-                $insert_attendance->current_status = 'I';
+	                $insert_attendance->name = $karyawan->NAMA_KARYAWAN;
+	                $insert_attendance->check_in = $now;
+	                $insert_attendance->child_analyst = $model->child_analyst;
+
+	                $location_data = WipLocation::find()
+	                ->where([
+	                    'child_analyst' => $model->child_analyst
+	                ])
+	                ->one();
+	                $insert_attendance->child_analyst_desc = $location_data->child_analyst_desc;
+	                $insert_attendance->current_status = 'I';
+	            } else {
+	            	$insert_attendance = $find_data;
+	                $insert_attendance->check_out = $now;
+	                if ($insert_attendance->current_status == 'I') {
+	                	$insert_attendance->current_status = 'O';
+	                } else {
+	                	$insert_attendance->current_status = 'I';
+	                }
+	            }
+	            $insert_attendance->line = $model->line;
+	            $insert_attendance->last_update = $now;
+
+	            if ($insert_attendance->save()) {
+	                $find_log = ProdAttendanceLog::find()
+	                ->where(['att_data_id' => $insert_attendance->att_data_id])
+	                ->orderBy('last_update DESC')
+	                ->one();
+
+	                $new_log = new ProdAttendanceLog;
+
+	                if ($find_log->att_log_id == null) {
+	                    $new_log->att_type = 'I';
+	                } else {
+	                    if ($new_log->att_type == 'I') {
+	                        $new_log->att_type = 'O';
+	                    }
+	                }
+	                $new_log->att_data_id = $insert_attendance->att_data_id;
+	                $new_log->last_update = $now;
+	                if (!$new_log->save()) {
+	                    return json_encode($new_log->errors);
+	                }
+	            } else {
+	                return json_encode($insert_attendance->errors);
+	            }
             } else {
-            	$insert_attendance = $find_data;
-                $insert_attendance->check_out = $now;
-                if ($insert_attendance->current_status == 'I') {
-                	$insert_attendance->current_status = 'O';
-                } else {
-                	$insert_attendance->current_status = 'I';
-                }
+            	\Yii::$app->session->setFlash("danger", "NIK is not registered ...");
             }
-            $insert_attendance->line = $model->line;
-            $insert_attendance->last_update = $now;
 
-            if ($insert_attendance->save()) {
-                $find_log = ProdAttendanceLog::find()
-                ->where(['att_data_id' => $insert_attendance->att_data_id])
-                ->orderBy('last_update DESC')
-                ->one();
-
-                if ($find_log->att_log_id == null) {
-                    $find_log = new ProdAttendanceLog;
-                    $find_log->att_type = 'I';
-                } else {
-                    if ($find_log->att_type == 'I') {
-                        $find_log->att_type = 'O';
-                    }
-                }
-                $find_log->att_data_id = $insert_attendance->att_data_id;
-                $find_log->last_update = $now;
-                if (!$find_log->save()) {
-                    return json_encode($find_log->errors);
-                }
-            } else {
-                return json_encode($insert_attendance->errors);
-            }
+            
             $model->nik = null;
         }
 
@@ -106,11 +114,37 @@ class ProdAttendanceController extends Controller
             'posting_shift' => $posting_shift
         ])
         ->orderBy('last_update DESC')
+        ->limit(10)
         ->all();
+
+        $attendance_log = ProdAttendanceLog::find()
+        ->select([
+        	'nik' => 'PROD_ATTENDANCE_DATA.nik', 'name' => 'PROD_ATTENDANCE_DATA.name', 'att_type', 'PROD_ATTENDANCE_LOG.last_update'
+        ])
+        ->joinWith('prodAttendanceData')
+        ->where([
+        	'PROD_ATTENDANCE_DATA.child_analyst' => $model->child_analyst,
+            'PROD_ATTENDANCE_DATA.line' => $model->line,
+            'PROD_ATTENDANCE_DATA.posting_shift' => $posting_shift
+        ])
+        ->orderBy('PROD_ATTENDANCE_LOG.last_update DESC')
+        ->limit(10)
+        ->all();
+
+        $total_mp = ProdAttendanceData::find()
+        ->where([
+            'child_analyst' => $model->child_analyst,
+            'line' => $model->line,
+            'posting_shift' => $posting_shift,
+            'current_status' => 'I'
+        ])
+        ->count();
 
         return $this->render('index', [
             'model' => $model,
             'attendance_data' => $attendance_data,
+            'attendance_log' => $attendance_log,
+            'total_mp' => $total_mp,
         ]);
 	}
 }
