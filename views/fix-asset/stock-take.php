e52bcde55ac9ed5626babbc37302746b6db4f228
frenkycp98 <friendq.c.p@gmail.com>
2019-11-20 09:16:36 +0700

    - change title - miinor change

--- views/fix-asset/stock-take.php
+++ views/fix-asset/stock-take.php
@@ -9,10 +9,10 @@ $this->title = [
     'breadcrumbs_title' => 'STOCK TAKING FIXED ASSET'
 ];
 
-$this->registerCssFile("@web/css/data_table.css");
+//$this->registerCssFile("@web/css/data_table.css");
 $this->registerCss(".japanesse { font-family: 'MS PGothic', Osaka, Arial, sans-serif; }
 	#dept-text {font-size: 1.2em; font-weight: bold; padding-bottom: 20px;}
-	th, td {font-weight: normal; font-size: 0.9em; padding: 10px; vertical-align: middle !important;}
+	th, td {font-weight: normal; font-size: 0.9em; padding: 10px; vertical-align: middle !important; letter-spacing: 1px;}
 	.date-format {min-width: 90px;}
 	#stock-take-container {padding: 10px 0px 20px 0px;}
 	.img-content {border: 1px solid Silver;}
@@ -78,81 +78,75 @@ $this->registerCss(".japanesse { font-family: 'MS PGothic', Osaka, Arial, sans-s
 						</p>
 					</div>
 				</div>
-			</div>
-		</div>
-	</div>
-</div>
-
-<div class="row">
-	<div class="col-md-12">
-		<div class="box box-default box-solid">
-			<div class="box-header">
-				<h3 class="box-title">Data History</h3>
-			</div>
-			<div class="box-body no-padding">
-				<table class="table table-responsice table-bordered table-striped">
-					<thead>
-						<tr>
-							<th class="text-center">No.</th>
-							<th>Detail</th>
-							<th class="text-center">Acquisition Date</th>
-							<th>Vendor</th>
-							<th class="text-center">Voucher Number</th>
-							<th class="text-center">Payment Date</th>
-							<th class="text-center">Depr. Date</th>
-							<th class="text-center">Qty</th>
-							<th class="text-center">Price</th>
-							<th class="text-center">Currency</th>
-							<th class="text-center">Rate</th>
-							<th class="text-center">At Cost</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php
-						if (!$asset_dtr) {
-							echo '<tr>
-								<td colspan="12">No history data ...</td>
-							</tr>';
-						} else {
-							$no = 1;
-							foreach ($asset_dtr as $key => $value) {
-								if ($value->dateacqledger == null) {
-									$acq_date = '-';
+				<hr>
+				<div class="row">
+					<div class="col-md-12">
+						<h4>Sub Expense</h4>
+						<table class="table table-responsice table-bordered table-striped">
+							<thead>
+								<tr class="bg-navy color-palette">
+									<th class="text-center">No.</th>
+									<th>Detail</th>
+									<th class="text-center">Acquisition Date</th>
+									<th>Vendor</th>
+									<th class="text-center">Voucher Number</th>
+									<th class="text-center">Payment Date</th>
+									<th class="text-center">Depr. Date</th>
+									<th class="text-center">Qty</th>
+									<th class="text-center">Price</th>
+									<th class="text-center">Currency</th>
+									<th class="text-center">Rate</th>
+									<th class="text-center">At Cost</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php
+								if (!$asset_dtr) {
+									echo '<tr>
+										<td colspan="12">No history data ...</td>
+									</tr>';
 								} else {
-									$acq_date = date('Y-m-d', strtotime($value->dateacqledger));
-								}
+									$no = 1;
+									foreach ($asset_dtr as $key => $value) {
+										if ($value->dateacqledger == null) {
+											$acq_date = '-';
+										} else {
+											$acq_date = date('Y-m-d', strtotime($value->dateacqledger));
+										}
 
-								if ($value->date_of_payment == null) {
-									$payment_date = '-';
-								} else {
-									$payment_date = date('Y-m-d', strtotime($value->date_of_payment));
-								}
+										if ($value->date_of_payment == null) {
+											$payment_date = '-';
+										} else {
+											$payment_date = date('Y-m-d', strtotime($value->date_of_payment));
+										}
 
-								if ($value->depr_date == null) {
-									$depr_date = '-';
-								} else {
-									$depr_date = date('Y-m-d', strtotime($value->depr_date));
-								}
+										if ($value->depr_date == null) {
+											$depr_date = '-';
+										} else {
+											$depr_date = date('Y-m-d', strtotime($value->depr_date));
+										}
 
-								echo '<tr>
-									<td class="text-center">' . $value->fixed_asset_subid . '</td>
-									<td>' . $value->description . '</td>
-									<td class="text-center date-format">' . $acq_date . '</td>
-									<td>' . $value->vendor . '</td>
-									<td class="text-center">' . $value->voucher_number . '</td>
-									<td class="text-center date-format">' . $payment_date . '</td>
-									<td class="text-center date-format">' . $depr_date . '</td>
-									<td class="text-center">' . number_format($value->qty) . '</td>
-									<td class="text-center">' . number_format($value->price_unit) . '</td>
-									<td class="text-center">' . $value->currency . '</td>
-									<td class="text-center">' . number_format($value->rate) . '</td>
-									<td class="text-center">' . number_format($value->at_cost) . '</td>
-								</tr>';
-							}
-						}
-						?>
-					</tbody>
-				</table>
+										echo '<tr>
+											<td class="text-center">' . $value->fixed_asset_subid . '</td>
+											<td>' . $value->description . '</td>
+											<td class="text-center date-format">' . $acq_date . '</td>
+											<td>' . $value->vendor . '</td>
+											<td class="text-center">' . $value->voucher_number . '</td>
+											<td class="text-center date-format">' . $payment_date . '</td>
+											<td class="text-center date-format">' . $depr_date . '</td>
+											<td class="text-center">' . number_format($value->qty) . '</td>
+											<td class="text-center">' . number_format($value->price_unit) . '</td>
+											<td class="text-center">' . $value->currency . '</td>
+											<td class="text-center">' . number_format($value->rate) . '</td>
+											<td class="text-center">' . number_format($value->at_cost) . '</td>
+										</tr>';
+									}
+								}
+								?>
+							</tbody>
+						</table>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
