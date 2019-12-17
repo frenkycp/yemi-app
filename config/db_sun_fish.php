4036e120ace437e8beab1f16ab9dbfe327720cb6
frenkycp98 <friendq.c.p@gmail.com>
2019-12-17 13:36:29 +0700

    - add db

---
+++ config/db_sun_fish.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'sqlsrv:Server=172.17.144.11;Database=dbSF_YAMAHAHRIS',
+    'username' => 'yemi',
+    'password' => 'Yem!#2019',
+    /*'dsn' => 'sqlsrv:Server=FRENKY-GLORIA\SQLEXPRESS;Database=PROD',
+    'username' => 'sa',
+    'password' => 'fg0908',*/
+    //'dsn' => 'dblib:host=localhost;Database=TEST',
+    //'username' => 'sa',
+    //'password' => 'f12345',
+    'charset' => 'utf8',
+    //'tablePrefix' => 'db_owner.',
+];
