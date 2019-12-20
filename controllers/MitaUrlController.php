55975a91046abb5d4570a386ae27ab624b0e3df9
frenkycp98 <friendq.c.p@gmail.com>
2019-12-20 13:51:12 +0700

    - no duplicate url

--- controllers/MitaUrlController.php
+++ controllers/MitaUrlController.php
@@ -6,6 +6,7 @@ use app\models\search\MitaUrlSearch;
 use yii\helpers\Url;
 use yii\filters\AccessControl;
 use dmstr\bootstrap\Tabs;
+use app\models\MitaUrl;
 
 class MitaUrlController extends \app\controllers\base\MitaUrlController
 {
@@ -25,5 +26,53 @@ class MitaUrlController extends \app\controllers\base\MitaUrlController
 			'dataProvider' => $dataProvider,
 		    'searchModel' => $searchModel,
 		]);
+	}
+
+	public function actionCreate()
+	{
+		$model = new MitaUrl;
+
+		try {
+			if ($model->load($_POST)) {
+				$tmp_url = MitaUrl::find()->where(['url' => $model->url])->one();
+				if ($tmp_url) {
+					\Yii::$app->session->setFlash("danger", "Url already exist ...");
+				} else {
+					if ($model->save()) {
+						return $this->redirect(Url::previous());
+					} else {
+						return json_encode($model->errors);
+					}
+				}
+			} elseif (!\Yii::$app->request->isPost) {
+				$model->load($_GET);
+			}
+		} catch (\Exception $e) {
+			$msg = (isset($e->errorInfo[2]))?$e->errorInfo[2]:$e->getMessage();
+			$model->addError('_exception', $msg);
+		}
+		return $this->render('create', ['model' => $model]);
+	}
+
+	public function actionUpdate($id)
+	{
+		$model = $this->findModel($id);
+
+		if ($model->load($_POST)) {
+			$tmp_url = MitaUrl::find()->where(['url' => $model->url])->one();
+			if ($tmp_url) {
+				\Yii::$app->session->setFlash("danger", "Url already exist ...");
+			} else {
+				if ($model->save()) {
+					return $this->redirect(Url::previous());
+				} else {
+					return json_encode($model->errors);
+				}
+			}
+		} else {
+			return $this->render('update', [
+				'model' => $model,
+			]);
+		}
 	}
 }
