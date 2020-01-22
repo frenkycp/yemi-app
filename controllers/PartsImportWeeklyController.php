4ae0a7c997380538b4d382f456113c5036c60acd
frenkycp98 <friendq.c.p@gmail.com>
2020-01-22 11:43:34 +0700

    - add parts import

---
+++ controllers/PartsImportWeeklyController.php
@@ -0,0 +1,212 @@
+<?php
+namespace app\controllers;
+
+use yii\web\Controller;
+use app\models\BookingShipTrackView;
+use app\models\BookingShipTrack02;
+
+class PartsImportWeeklyController extends Controller
+{
+	public function behaviors()
+    {
+        //apply role_action table for privilege (doesn't apply to super admin)
+        return \app\models\Action::getAccess($this->id);
+    }
+
+    public function actionIndex()
+    {
+        date_default_timezone_set('Asia/Jakarta');
+    	$title = '';
+    	$subtitle = '';
+    	$data = [];
+    	$trans_method = [
+            'HCW', 'CCW', 'JCW', 'SZCW', 'PKL', 'PNG', 'YTS'
+        ];
+
+        $month_arr = [];
+
+        for ($month = 1; $month <= 12; $month++) {
+            $month_arr[date("m", mktime(0, 0, 0, $month, 10))] = date("F", mktime(0, 0, 0, $month, 10));
+        }
+
+        $period_model = new \yii\base\DynamicModel([
+            'year', 'month'
+        ]);
+
+        $period_model->addRule(['year','month'], 'required')
+        ->addRule(['year', 'month'], 'string');
+
+        $period_model->month = date('m');
+        $period_model->year = date('Y');
+
+        if($period_model->load(\Yii::$app->request->get())){
+            // do somenthing with model
+            //return $this->redirect(['view']);
+        }
+
+        $period = $period_model->year . $period_model->month;
+
+    	$global_condition = [
+    		'TRANS_MTHD' => $trans_method,
+            'PERIOD' => $period
+		];
+
+    	$week_arr = $this->getWeekArr($global_condition);
+    	$today = new \DateTime(date('Y-m-d'));
+		$this_week = $today->format("W");
+		if (!in_array($this_week, $week_arr)) {
+			$this_week = end($week_arr);
+		}
+    	
+    	$booking_data_arr = BookingShipTrackView::find()
+    	->select([
+    		'WEEK',
+    		'DATE',
+    		'total_open' => 'SUM(CASE WHEN STAT_02 = \'O\' AND STAT_ID NOT IN (3, 4) THEN ORDER_QTY ELSE 0 END)',
+            'total_open2' => 'SUM(CASE WHEN STAT_02 = \'O\' AND STAT_ID IN (3, 4) THEN ORDER_QTY ELSE 0 END)',
+    		'total_close' => 'SUM(CASE WHEN STAT_02 = \'C\' THEN ORDER_QTY ELSE 0 END)'
+    	])
+    	->where($global_condition)
+    	->groupBy('WEEK, DATE')
+    	->orderBy('DATE ASC')
+    	->all();
+
+    	foreach ($week_arr as $week_no) {
+    		$tmp_category = [];
+    		$tmp_data = [];
+    		$tmp_data_open = [];
+            $tmp_data_open2 = [];
+    		$tmp_data_close = [];
+    		foreach ($booking_data_arr as $booking_data) {
+    			if ($week_no == $booking_data->WEEK) {
+    				$tmp_category[] = $booking_data->DATE;
+    				$open_qty = (int)$booking_data->total_open;
+                    $open_qty2 = (int)$booking_data->total_open2;
+    				$close_qty = (int)$booking_data->total_close;
+    				$order_qty = $open_qty + $close_qty;
+
+    				$tmp_data_open[] = [
+    					'y' => $open_qty == 0 ? null : $open_qty,
+    					//'y' => $open_qty == 0 ? null : $open_qty,
+    					'remark' => $this->getRemark('STAT_02 = \'O\' AND STAT_ID NOT IN (3, 4)', $booking_data->DATE, $trans_method, 0)
+    				];
+                    $tmp_data_open2[] = [
+                        'y' => $open_qty2 == 0 ? null : $open_qty2,
+                        'remark' => $this->getRemark('STAT_02 = \'O\' AND STAT_ID IN (3, 4)', $booking_data->DATE, $trans_method, 0)
+                    ];
+    				$tmp_data_close[] = [
+    					'y' => $close_qty == 0 ? null : $close_qty,
+    					//'y' => $close_qty == 0 ? null : $close_qty,
+    					'remark' => $this->getRemark('STAT_02 = \'C\'', $booking_data->DATE, $trans_method, 0)
+    				];
+    			}
+    		}
+    		$data[$week_no][] = [
+    			'category' => $tmp_category,
+    			'data' => [
+    				[
+    					'name' => 'Prepared',
+    					'data' => $tmp_data_open,
+    					'color' => 'rgba(200, 200, 200, 0.8)',
+    					//'showInLegend' => false,
+    				],
+                    [
+                        'name' => 'Partially Departed & Received',
+                        'data' => $tmp_data_open2,
+                        'color' => 'rgba(255, 200, 0, 0.8)',
+                        //'showInLegend' => false,
+                    ],
+    				[
+    					'name' => 'Fully Departed & Received',
+    					'data' => $tmp_data_close,
+    					'color' => 'rgba(0, 200, 0, 0.8)',
+    					//'showInLegend' => false,
+    				],
+    			]
+    		];
+    	}
+
+    	return $this->render('index', [
+    		'title' => $title,
+    		'subtitle' => $subtitle,
+    		'data' => $data,
+    		'this_week' => $this_week,
+            'period_model' => $period_model,
+            'month_arr' => $month_arr
+    	]);
+    }
+
+    public function getWeekArr($global_condition)
+    {
+    	$return_arr = [];
+    	$data_arr = BookingShipTrackView::find()
+    	->select('WEEK')
+    	->where($global_condition)
+    	->groupBy('WEEK')
+    	->orderBy('WEEK')
+    	->all();
+
+    	foreach ($data_arr as $value) {
+    		$return_arr[] = $value->WEEK;
+    	}
+
+    	return $return_arr;
+    }
+
+    public function getRemark($bo_status, $date, $trans_method, $stat_id)
+    {
+    	$data_arr = BookingShipTrack02::find()
+    	->where([
+    		'TRANS_MTHD' => $trans_method,
+    		//'STAT_ID' => $stat_id
+    	])
+    	->andWhere($bo_status)
+    	->andWhere(['like', 'CONVERT(VARCHAR(10),PICKUP_ACTUAL,120)', $date])
+    	->orderBy('BOOKING_ID ASC')
+    	->all();
+
+    	$data = '<table class="table table-bordered table-striped table-hover">';
+		$data .= 
+		'<tr>
+			<th class="text-center">Booking ID</th>
+			<th>User Description</th>
+			<th class="text-center">Pickup Actual</th>
+            <th class="text-center" style="width:80px;">YEMI Arrival</th>
+			<th>Shipper</th>
+			<th class="text-center">Item</th>
+			<th>Item Description</th>
+			<th class="text-center">Order Qty</th>
+			<th class="text-center">Rcv Qty</th>
+			<th class="text-center">BO Qty</th>
+            <th>Status</th>
+		</tr>'
+		;
+
+		foreach ($data_arr as $value) {
+            $yemi_arrival_date = $value['YEMI_ARRIVAL'] == null ? '-' : date('Y-m-d', strtotime($value['YEMI_ARRIVAL']));
+            $pickup_actual = '-';
+            if (date('H:i:s', strtotime($value['PICKUP_ACTUAL'])) != '00:00:00') {
+                $pickup_actual = $value['PICKUP_ACTUAL'];
+            }
+			$data .= '
+				<tr>
+					<td class="text-center">' . $value['BOOKING_ID'] . '</td>
+					<td>' . $value['USER_DESC'] . '</td>
+					<td class="text-center">' . $pickup_actual . '</td>
+                    <td class="text-center">' . $yemi_arrival_date . '</td>
+					<td>' . $value['SHIPPER'] . '</td>
+					<td class="text-center">' . $value['ITEM'] . '</td>
+					<td>' . $value['ITEM_DESC'] . '</td>
+					<td class="text-center">' . number_format($value['ORDER_QTY']) . '</td>
+					<td class="text-center">' . number_format($value['RCV_QTY']) . '</td>
+					<td class="text-center">' . number_format(abs($value['BO_QTY'])) . '</td>
+                    <td class="text-center">' . $value['STAT_ID_DESC'] . '</td>
+				</tr>
+				';
+		}
+
+		$data .= '</table>';
+		return $data;
+    }
+
+}
---
+++ views/parts-import-weekly/index.php
@@ -0,0 +1,190 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use miloschuman\highcharts\Highcharts;
+use yii\web\JsExpression;
+use yii\web\View;
+use yii\bootstrap\ActiveForm;
+
+/**
+* @var yii\web\View $this
+* @var yii\data\ActiveDataProvider $dataProvider
+* @var app\models\search\MenuSearch $searchModel
+*/
+
+$this->title = [
+    'page_title' => 'Weekly Import Parts  <span class="text-green"></span>',
+    'tab_title' => 'Weekly Import Parts',
+    'breadcrumbs_title' => 'Weekly Import Parts'
+];
+//$this->params['breadcrumbs'][] = $this->title['breadcrumbs_title'];
+date_default_timezone_set('Asia/Jakarta');
+
+$this->registerCss("
+    h1 span { 
+        font-family: 'MS PGothic', Osaka, Arial, sans-serif; 
+    }
+    table {
+        font-size: 12px;
+    }
+");
+
+$script = <<< JS
+    window.onload = setupRefresh;
+
+    function setupRefresh() {
+      setTimeout("refreshPage();", 300000); // milliseconds
+    }
+    function refreshPage() {
+       window.location = location.href;
+    }
+JS;
+$this->registerJs($script, View::POS_HEAD );
+
+/*echo '<pre>';
+print_r($data);
+echo '</pre>';*/
+?>
+
+<?php $form = ActiveForm::begin([
+    'method' => 'get'
+]); ?>
+
+    <div class="row">
+        <div class="col-md-2">
+            <?= $form->field($period_model, 'year')->dropDownList(
+                \Yii::$app->params['year_arr'],
+                [
+                    'onchange'=>'this.form.submit()'
+                ]
+            ) ?>
+        </div>
+        <div class="col-md-2">
+            <?= $form->field($period_model, 'month')->dropDownList(
+                $month_arr,
+                [
+                    'onchange'=>'this.form.submit()'
+                ]
+            ) ?>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <?= '';Html::submitButton('Update Chart', ['class' => 'btn btn-primary']) ?>
+    </div>
+
+<?php ActiveForm::end(); ?>
+<h4 class="box-title"><i class="fa fa-tag"></i> Last Update : <?= date('Y-m-d H:i:s') ?></h4>
+<div class="nav-tabs-custom">
+    <ul class="nav nav-tabs">
+        <?php
+        foreach ($data as $key => $value) {
+            if($key == $this_week)
+            {
+                echo '<li class="active"><a href="#tab_1_' . $key . '" data-toggle="tab">Week ' . $key . '</a></li>';
+            }
+            else
+            {
+                echo '<li><a href="#tab_1_' . $key . '" data-toggle="tab">Week ' . $key . '</a></li>';
+            }
+        }
+        ?>
+    </ul>
+    <div class="tab-content">
+        <?php
+        foreach ($data as $key => $value) {
+            if($key == $this_week)
+            {
+                echo '<div class="tab-pane active" id="tab_1_' . $key .'">';
+            }
+            else
+            {
+                echo '<div class="tab-pane" id="tab_1_' . $key .'">';
+            }
+
+            echo Highcharts::widget([
+                'scripts' => [
+                    'modules/exporting',
+                    'themes/grid-light',
+                ],
+                'options' => [
+                    'chart' => [
+                        'type' => 'column',
+                        'height' => 350,
+                        'style' => [
+                            'fontFamily' => 'Source Sans Pro'
+                        ],
+                    ],
+                    'credits' => [
+                        'enabled' =>false
+                    ],
+                    'title' => [
+                        'text' => ''
+                    ],
+                    'subtitle' => [
+                        'text' => ''
+                    ],
+                    'xAxis' => [
+                        'type' => 'category',
+                        'categories' => $value[0]['category'],
+                    ],
+                    'yAxis' => [
+                        'min' => 0,
+                        'title' => [
+                            'text' => 'Total Completion'
+                        ],
+                        //'gridLineWidth' => 0,
+                    ],
+                    'tooltip' => [
+                        'enabled' => true,
+                        'valueSuffix' => ' pcs'
+                        //'formatter' => new JsExpression('function(){ return "Percentage : " + this.y + "%<br/>" + "Qty : " + this.point.qty + " pcs"; }'),
+                    ],
+                    'plotOptions' => [
+                        'column' => [
+                            'stacking' => 'percent',
+                            'dataLabels' => [
+                                'enabled' => true,
+                                'format' => '{point.percentage:.1f}%',
+                                'color' => 'black',
+                                'allowOverlap' => true,
+                                //'formatter' => new JsExpression('function(){ if(this.y != 0) { return this.y; } }'),
+                                'style' => [
+                                    //'fontSize' => '14px',
+                                    'textOutline' => '0px',
+                                    'fontWeight' => '0'
+                                ],
+                            ],
+                            //'borderWidth' => 1,
+                            //'borderColor' => $color,
+                        ],
+                        'series' => [
+                            'cursor' => 'pointer',
+                            'point' => [
+                                'events' => [
+                                    'click' => new JsExpression('
+                                        function(){
+                                            $("#modal").modal("show").find(".modal-body").html(this.options.remark);
+                                        }
+                                    '),
+                                ]
+                            ]
+                        ]
+                    ],
+                    'series' => $value[0]['data']
+                ],
+            ]);
+
+            echo '</div>';
+        }
+
+        yii\bootstrap\Modal::begin([
+            'id' =>'modal',
+            'header' => '<h3>Detail Information</h3>',
+            'size' => 'modal-lg',
+        ]);
+        yii\bootstrap\Modal::end();
+        ?>
+    </div>
+</div>
