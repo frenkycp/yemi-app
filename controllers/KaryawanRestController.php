b3082e255e45b0927fa0bcb7bb99e659ee2b9029
FRENKY-GLORIA\Frenky - Gloria <friendq.c.p@gmail.com>
2020-03-24 10:22:00 +0700

    - add rest controller for data karyawan

---
+++ controllers/KaryawanRestController.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\controllers;
+
+use yii\rest\Controller;
+use app\models\Karyawan;
+
+class KaryawanRestController extends Controller
+{
+    public function actionGetInfo($nik)
+    {
+    	\Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+    	$tmp_karyawan = Karyawan::find()
+    	->select(['NIK', 'NIK_SUN_FISH', 'NAMA_KARYAWAN', 'DEPARTEMEN', 'SECTION'])
+    	->where(['OR', ['NIK_SUN_FISH' => $nik], ['NIK' => $nik]])->one();
+
+    	if (!$tmp_karyawan) {
+    		return 'NIK not found';
+    	}
+
+    	return $tmp_karyawan;
+    }
+}
