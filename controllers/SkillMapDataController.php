134db390e38bc254ff9538434549cc1ca4f4b6d4
frenkycp98 <friendq.c.p@gmail.com>
2020-07-06 07:55:36 +0700

    - update skill map from sunfish db

--- controllers/SkillMapDataController.php
+++ controllers/SkillMapDataController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use app\models\Karyawan;
 use app\models\SkillMaster;
 use app\models\SkillMasterKaryawan;
+use app\models\SunfishViewEmp;
 use app\models\SernoMaster;
 use yii\helpers\ArrayHelper;
 /**
@@ -65,38 +66,51 @@ class SkillMapDataController extends \app\controllers\base\SkillMapDataControlle
                     ])
                     ->one();
 
-                    if ($tmp_skill_master->NIK == null) {
-                        $sql = "{CALL SKILL_CREATE(:NIK, :USER_ID, :USER_DESC)}";
+                    $karyawan_for_update = SunfishViewEmp::find()
+                    ->select('Emp_no, Full_name')
+                    ->where([
+                        'Emp_no' => $value
+                    ])
+                    ->one();
+
+                    if ($karyawan_for_update->Emp_no == NULL) {
+                        \Yii::$app->session->addFlash("danger", 'NIK is not registered at employee database. Please contact HR to update database...');
+                    } else {
+                        if ($tmp_skill_master->NIK == null) {
+                            $sql = "{CALL SKILL_CREATE(:NIK, :USER_ID, :USER_DESC)}";
+                            $params = [
+                                ':NIK' => $value,
+                                ':USER_ID' => $tmp_karyawan->NIK_SUN_FISH,
+                                ':USER_DESC' => $tmp_karyawan->NAMA_KARYAWAN,
+                            ];
+
+                            try {
+                                $result = \Yii::$app->db_sql_server->createCommand($sql, $params)->execute();
+                                \Yii::$app->session->addFlash("warning", $value . ' - ' . $karyawan_for_update->Full_name . ' doesn\'t have skill map data. Default skill map has been added for this user.');
+                                //\Yii::$app->session->setFlash('success', 'Slip number : ' . $value . ' has been completed ...');
+                            } catch (Exception $ex) {
+                                \Yii::$app->session->addFlash('danger', "Error : $ex");
+                            }
+                        }
+                        $sql = "{CALL SKILL_UPDATE(:NIK, :skill_id, :skill_value, :USER_ID, :USER_DESC)}";
                         $params = [
                             ':NIK' => $value,
+                            ':skill_id' => $splitted_skill[0],
+                            ':skill_value' => $model->skill_value,
                             ':USER_ID' => $tmp_karyawan->NIK_SUN_FISH,
                             ':USER_DESC' => $tmp_karyawan->NAMA_KARYAWAN,
                         ];
 
                         try {
                             $result = \Yii::$app->db_sql_server->createCommand($sql, $params)->execute();
-                            \Yii::$app->session->addFlash("warning", $value . ' - ' . $tmp_skill_master->NAMA_KARYAWAN . ' doesn\'t have skill map data. Default skill map has been added for this user.');
+                            \Yii::$app->session->addFlash("success", 'Skill for ' . $value . ' - ' . $karyawan_for_update->Full_name . ' has been updated...');
                             //\Yii::$app->session->setFlash('success', 'Slip number : ' . $value . ' has been completed ...');
                         } catch (Exception $ex) {
                             \Yii::$app->session->addFlash('danger', "Error : $ex");
                         }
                     }
-                    $sql = "{CALL SKILL_UPDATE(:NIK, :skill_id, :skill_value, :USER_ID, :USER_DESC)}";
-                    $params = [
-                        ':NIK' => $value,
-                        ':skill_id' => $splitted_skill[0],
-                        ':skill_value' => $model->skill_value,
-                        ':USER_ID' => $tmp_karyawan->NIK_SUN_FISH,
-                        ':USER_DESC' => $tmp_karyawan->NAMA_KARYAWAN,
-                    ];
 
-                    try {
-                        $result = \Yii::$app->db_sql_server->createCommand($sql, $params)->execute();
-                        \Yii::$app->session->addFlash("success", 'Skill for ' . $value . ' - ' . $tmp_skill_master->NAMA_KARYAWAN . ' has been updated...');
-                        //\Yii::$app->session->setFlash('success', 'Slip number : ' . $value . ' has been completed ...');
-                    } catch (Exception $ex) {
-                        \Yii::$app->session->addFlash('danger', "Error : $ex");
-                    }
+                    
                 }
 	        	
         	}
