5c7f72ba1b851714acbbf8c2f579ca00e58d4bc2
frenkycp98 <friendq.c.p@gmail.com>
2020-07-14 15:24:07 +0700

    - add shipping display

--- controllers/DisplayController.php
+++ controllers/DisplayController.php
@@ -143,9 +143,128 @@ use app\models\DnsStatus;
 use app\models\VmsPlanActual;
 use app\models\ServerBackupCurrent;
 use app\models\HakAksesPlus;
+use app\models\ContainerView;
 
 class DisplayController extends Controller
 {
+    public function actionShippingDisplay($value='')
+    {
+        $this->layout = 'clean';
+        date_default_timezone_set('Asia/Jakarta');
+        $today = date('Y-m-d');
+        $tmp_yesterday = WorkDayTbl::find()
+        ->select([
+            'cal_date' => 'FORMAT(cal_date, \'yyyy-MM-dd\')'
+        ])
+        ->where([
+            '<', 'FORMAT(cal_date, \'yyyy-MM-dd\')', $today
+        ])
+        ->orderBy('cal_date DESC')
+        ->one();
+        $yesterday = $tmp_yesterday->cal_date;
+        $tmp_tomorrow = WorkDayTbl::find()
+        ->select([
+            'cal_date' => 'FORMAT(cal_date, \'yyyy-MM-dd\')'
+        ])
+        ->where([
+            '>', 'FORMAT(cal_date, \'yyyy-MM-dd\')', $today
+        ])
+        ->orderBy('cal_date')
+        ->one();
+        $tomorrow = $tmp_tomorrow->cal_date;
+
+        $data = [
+            $yesterday => [],
+            $today => [],
+            $tomorrow => [],
+        ];
+
+        $data_table = [];
+
+        foreach ($data as $key => $value) {
+            $tmp_so = SernoOutput::find()
+            ->select([
+                'qty' => 'SUM(qty)',
+                'output' => 'SUM(output)',
+                'balance' => 'SUM(output - qty)',
+            ])
+            ->where([
+                'etd' => $key
+            ])
+            ->one();
+            $total_container = SernoOutput::find()
+            ->select([
+                'cntr'
+            ])
+            ->where([
+                'etd' => $key
+            ])
+            ->groupBy('cntr')
+            ->count();
+            $data_table[] = [
+                'plan' => $tmp_so->qty,
+                'actual' => $tmp_so->output,
+                'balance' => $tmp_so->balance,
+                'total_container' => $total_container,
+            ];
+        }
+
+        $etd = date('Y-m-d');
+        $container = ContainerView::find()->where(['etd' => $etd])->orderBy('dst ASC')->all();
+        $total_container = 0;
+        $total_airshipment = 0;
+
+        foreach ($container as $key => $value) {
+            $close_percentage = 0;
+            if ($value->qty > 0) {
+                $close_percentage = (int)floor(($value->output / $value->qty) * 100);
+            }
+            
+            $open_percentage = (int)(100 - $close_percentage);
+            $dataOpen[] = [
+                'y' => $open_percentage > 0 ? $open_percentage : null,
+                'qty' => $value->balance,
+                'url' => Url::to(['serno-output/index', 'index_type' => 1, 'etd' => $value->etd, 'dst' => $value->dst, 'back_order' => $value->back_order]),
+            ];
+            $dataClose[] = [
+                'y' => $close_percentage > 0 ? $close_percentage : null,
+                'qty' => $value->output,
+                'url' => Url::to(['serno-output/index', 'index_type' => 2, 'etd' => $value->etd, 'dst' => $value->dst, 'back_order' => $value->back_order]),
+            ];
+            //$dataOpen[] = 50;
+            //$dataClose[] = 50;
+            $str_container = '_container)';
+            if ($value->back_order == 2) {
+                $str_container = '_airshipment)';
+            }
+            $str_airplane = '_airplane';
+            if($value->total_cntr > 1)
+            {
+                $str_container = '_containers)';
+                if ($value->back_order == 2) {
+                    $str_container = '_airshipments)';
+                }
+            }
+            $total_container += $value->back_order != 2 ? $value->total_cntr : 0;
+            $total_airshipment += $value->back_order == 2 ? $value->total_cntr : 0;
+            $dataName[] = $value->dst . ' (' . $value->total_cntr . $str_container;
+        }
+        //return json_encode($dataOpen);
+        $containerStr = $total_container . ' Container || ' . $total_airshipment . ' Air Shipment';
+        if($total_container > 1)
+        {
+            $containerStr = $total_container . ' Containers || ' . $total_airshipment . ' Air Shipments';
+        }
+
+        return $this->render('shipping-display', [
+            'data_table' => $data_table,
+            'dataOpen' => $dataOpen,
+            'dataClose' => $dataClose,
+            'dataName' => $dataName,
+            'containerStr' => $containerStr
+        ]);
+    }
+
     function actionVmsVsFlo($value='')
     {
         $this->layout = 'clean';
