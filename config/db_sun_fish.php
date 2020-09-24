9aaa8d24001435cfb62746333f0887c83b0395c1
DESKTOP-8HFAB5G\YEMIMIS04 <friendq.c.p@gmail.com>
2020-09-24 15:10:15 +0700

    - update db sunfich con

--- config/db_sun_fish.php
+++ config/db_sun_fish.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'sqlsrv:Server=172.17.144.11;Database=dbSF_YAMAHAHRIS',
+    'dsn' => 'sqlsrv:Server=10.110.48.8;Database=dbSF_YAMAHAHRIS',
     'username' => 'yemi',
     'password' => 'Yem!#2019',
     /*'dsn' => 'sqlsrv:Server=FRENKY-GLORIA\SQLEXPRESS;Database=PROD',
