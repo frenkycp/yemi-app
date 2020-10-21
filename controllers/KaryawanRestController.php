f2be660eb10d18f7eb6a278c110f242e8f0cee86
DESKTOP-8HFAB5G\YEMIMIS04 <friendq.c.p@gmail.com>
2020-10-21 10:23:55 +0700

    - add rest action for sunfish attendance data

--- controllers/KaryawanRestController.php
+++ controllers/KaryawanRestController.php
@@ -3,6 +3,7 @@ namespace app\controllers;
 
 use yii\rest\Controller;
 use app\models\Karyawan;
+use app\models\SunfishAttendanceData;
 
 class KaryawanRestController extends Controller
 {
@@ -27,5 +28,31 @@ class KaryawanRestController extends Controller
     	}
 
     	return $tmp_karyawan;
+    }
+
+    public function actionSunfishAttendance($from_date = '', $to_date = '')
+    {
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+
+        if ($from_date == '' || $to_date == '') {
+            $from_date = date('Y-m-01');
+            $to_date = date('Y-m-d');
+        }
+
+        $tmp_attendance = SunfishAttendanceData::find()
+        ->select([
+            'post_date' => 'CAST(shiftendtime AS DATE)', 'VIEW_YEMI_ATTENDANCE.emp_no', 'VIEW_YEMI_ATTENDANCE.full_name', 'cost_center', 'grade' => 'VIEW_YEMI_Emp_OrgUnit.grade_code', 'attend_judgement', 'starttime', 'endtime'
+        ])
+        ->leftJoin('VIEW_YEMI_Emp_OrgUnit', 'VIEW_YEMI_Emp_OrgUnit.Emp_no = VIEW_YEMI_ATTENDANCE.emp_no')
+        ->where([
+            'AND',
+            ['>=', 'FORMAT(shiftendtime, \'yyyy-MM-dd\')', $from_date],
+            ['<=', 'FORMAT(shiftendtime, \'yyyy-MM-dd\')', $to_date]
+        ])
+        ->orderBy('shiftendtime, cost_center, emp_no')
+        ->asArray()
+        ->all();
+
+        return $tmp_attendance;
     }
 }
