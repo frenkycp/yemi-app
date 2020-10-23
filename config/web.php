6a50046948447875425d9047378bc79e0f4da9c4
DESKTOP-8HFAB5G\YEMIMIS04 <friendq.c.p@gmail.com>
2020-10-23 14:30:34 +0700

    - add missing web configuration

--- config/web.php
+++ config/web.php
@@ -50,7 +50,15 @@ $config = [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'purnama.frenky@gmail.com',
+                'password' => 'experience0908',
+                'port' => '587',
+                'encryption' => 'tls'
+            ],
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
