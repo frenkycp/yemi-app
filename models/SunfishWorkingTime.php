17177e8aa7685a15663a24c48e8fb456b49fc1f5
YEMIMIS04\YEMIMIS_04 <friendq.c.p@gmail.com>
2021-06-17 15:09:44 +0700

    - add model

---
+++ models/SunfishWorkingTime.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use \app\models\base\SunfishWorkingTime as BaseSunfishWorkingTime;
+use yii\helpers\ArrayHelper;
+
+/**
+ * This is the model class for table "db_owner.SUNFISH_WORKING_TIME".
+ */
+class SunfishWorkingTime extends BaseSunfishWorkingTime
+{
+
+    public function behaviors()
+    {
+        return ArrayHelper::merge(
+            parent::behaviors(),
+            [
+                # custom behaviors
+            ]
+        );
+    }
+
+    public function rules()
+    {
+        return ArrayHelper::merge(
+            parent::rules(),
+            [
+                # custom validation rules
+            ]
+        );
+    }
+}
